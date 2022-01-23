b737c6dd0b2fd4e92c763dfd6fbcef5d5b4f0cbb
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-01-23 21:01:30 +0100

    add allowed hosts to settings.py

--- quarrel/settings.py
+++ quarrel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quarrel2022.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['quarrel2022.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
