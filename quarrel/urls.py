c45622a6907ebe853f5fef15c7ac8935705376f0
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-01-27 14:13:23 +0100

    install allauth

--- quarrel/urls.py
+++ quarrel/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quarrelapp.urls'), name='quarrelapp_urls')
+    path('', include('quarrelapp.urls'), name='quarrelapp_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
