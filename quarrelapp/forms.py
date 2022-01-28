3c650a676e53106dce2e358c96f10ea09490ba0e
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-01-28 21:05:27 +0100

    add crispy forms, added form, and implemented on signup and register, and post details

--- quarrelapp/forms.py
+++ quarrelapp/forms.py
@@ -1,8 +1,13 @@
-# from django import forms
-# from .models import Post
+from django import forms
+from .models import Comment
 
 
 # class PostForm(forms.ModelForm):
 #     class Meta:
 #         model = Post
 #         fields = ['content_one', 'content_two', 'title']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
