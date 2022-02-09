0c77c03dda750fc8d9419de50a5559dcb061e2a9
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-02-09 19:00:27 +0100

    configure commentpost and post in admin

--- quarrelapp/admin.py
+++ quarrelapp/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from .models import Post, Comment, CommentPost
 
 
-admin.site.register(Post)
-admin.site.register(CommentPost)
-
-
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'date_published')
     search_fields = ('name', 'email', 'body')
+
+
+@admin.register(CommentPost)
+class CommentPostAdmin(admin.ModelAdmin):
+    list_display = ('content',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'content_one', 'content_two',
+                    'date_published')
+    list_filter = ('user', 'title')
